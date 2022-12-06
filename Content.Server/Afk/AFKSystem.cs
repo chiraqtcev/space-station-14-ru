5d3529e5918a98c89254077ac25eb66ae64410e2
chiraqtcev <6996dead@mail.ru>
2022-12-06 13:58:45 +0400

    Перенос обновлений с основного репозитория

--- Content.Server/Afk/AFKSystem.cs
+++ Content.Server/Afk/AFKSystem.cs
@@ -85,6 +85,9 @@ public sealed class AFKSystem : EntitySystem
 
         foreach (var session in Filter.GetAllPlayers())
         {
+            if (session.Status != SessionStatus.InGame)
+                continue;
+
             var pSession = (IPlayerSession) session;
             var isAfk = _afkManager.IsAfk(pSession);
 
@@ -92,7 +95,7 @@ public sealed class AFKSystem : EntitySystem
             {
                 var ev = new AFKEvent(pSession);
                 RaiseLocalEvent(ref ev);
-                
+
                 _chatManager.DispatchServerMessage(pSession, Loc.GetString("afk-system-kick-warning"));
             }
 
